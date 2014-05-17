333f8a65c3a201c8ba0b5de36aa0f8c743463d87
luigi <007.lva@gmail.com>
2014-05-17 18:52:59 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joifeel::Application.config.secret_key_base = '4ab3ef44d88ced253ed7b67531cca844c5f7d036142c1c8d2ff1cc3a7f05ae092cb871f742acb918e868d4d5c5a062bef7f63b18264d4b0a0e0279dd38f5fa27'
