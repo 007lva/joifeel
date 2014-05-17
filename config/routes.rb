f1bee675c8d6c173c366895a0116502bde7a08bb
angelito rojas <angelsystem1387@gmail.com>
2014-05-17 12:53:11 -0500

    bootstrap css

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Joifeel::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'page_static#index'
+  root 'page_statics#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
